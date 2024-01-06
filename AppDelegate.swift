cc770b139b660b56fe2c00a0fc33a62e0c08960a
Rabia Ghafoor <57128418+Rabia-Ghafoor@users.noreply.github.com>
2024-01-06 00:07:51 -0500

    Add files via upload

---
+++ AppDelegate.swift
@@ -0,0 +1,83 @@
+//
+//  AppDelegate.swift
+//  Conchecks
+//
+//  Created by Rabia on 2020-11-23.
+//
+
+import UIKit
+import Parse
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let configuration = ParseClientConfiguration {
+          $0.applicationId = "Ayznn41gbRfpS6xjn66AQ5lQPhiARPPERdSQQA9h"
+          $0.clientKey = "cOEgtaqmKNOyyYrckW4rtmeUdXRRAP9hhARQsPTS"
+          $0.server = "https://pg-app-0aqrz09jxcmthzp9yewvlrpbhr146e.scalabl.cloud/1/"
+        }
+        Parse.initialize(with: configuration)
+        registerForPushNotifications()
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func registerForPushNotifications() {
+      UNUserNotificationCenter.current() // 1
+        .requestAuthorization(options: [.alert, .sound, .badge]) {
+            [weak self] granted, error in
+              
+            print("Permission granted: \(granted)")
+            guard granted else { return }
+            self?.getNotificationSettings()
+        }
+    }
+    
+    func getNotificationSettings() {
+      UNUserNotificationCenter.current().getNotificationSettings { settings in
+        print("Notification settings: \(settings)")
+        guard settings.authorizationStatus == .authorized else { return }
+        DispatchQueue.main.async {
+          UIApplication.shared.registerForRemoteNotifications()
+        }
+      }
+    }
+
+    func application(
+      _ application: UIApplication,
+      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+      let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+      let token = tokenParts.joined()
+      let installation = PFInstallation.current()
+        print(deviceToken)
+        installation!.setDeviceTokenFrom(deviceToken)
+        installation!.saveInBackground()
+      print("Device Token: \(token)")
+    }
+
+    func application(
+      _ application: UIApplication,
+      didFailToRegisterForRemoteNotificationsWithError error: Error) {
+      print("Failed to register: \(error)")
+    }
+
+
+}
+
